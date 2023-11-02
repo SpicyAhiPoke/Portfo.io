e112ae09d0ad9fea141cb774ff1485d33d352573
SpicyAhiPoke <shawnrccr@gmail.com>
2023-11-01 22:30:19 -0700

    Update ProjectService.cs

--- Portfolio/Data/ProjectService.cs
+++ Portfolio/Data/ProjectService.cs
@@ -14,7 +14,7 @@
 				new Project() {ProjectId = 3, Name = "Global Consulting Agency", Author = $"{myname}", Description = "An application scheduling desktop application for an organization that conducts business in multiple languages and has main offices worldwide.", Url = "", Complete = new DateOnly(2023, 05, 23)},
 				new Project() {ProjectId = 4, Name = "Taniti", Author = $"{myname}", Description = "A UX prototype of a tourism website for a local government.", Url = "https://sites.google.com/wgu.edu/taniti/", Complete = new DateOnly(2022, 08, 05)},
 				new Project() {ProjectId = 5, Name = "Data Migration", Author = $"{myname}", Description = "A periodic extraction of data from a larger database by creating a repeatable data export, import, and analysis for various stakeholders.", Url = "", Complete = new DateOnly(2022, 03, 17)},
-				new Project() {ProjectId = 6, Name = "Pet Professionals", Author = $"{myname}", Description = "As a UI designer, develop a responsive website for a pet consulting company.", Url = "", Complete = new DateOnly(2021, 11, 08)},
+				new Project() {ProjectId = 6, Name = "Pet Professionals", Author = $"{myname}", Description = "A responsive website UI design for a pet consulting company.", Url = "", Complete = new DateOnly(2021, 11, 08)},
 				new Project() {ProjectId = 7, Name = "Inventory Management System", Author = $"{myname}", Description = "A inventory desktop application for a small manufacturing organization.", Url = "", Complete = new DateOnly(2021, 09, 30)},
 				new Project() {ProjectId = 8, Name = "Class Roster", Author = $"{myname}", Description = "Data migration of an existing University student roster.", Url = "", Complete = new DateOnly(2020, 09, 11)},
 			};
